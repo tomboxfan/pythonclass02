0286ca76a2e0843040f479429dffebb0f6f79f4a
Tom Fan <tomboxfan@hotmail.com>
2021-03-13 22:09:59 +0800

    lesson 4

--- program1/python_0017_input.py
+++ program1/python_0017_input.py
@@ -6,7 +6,7 @@ your_name = input("Your name: ")
 # As soon as the program runs to the line input(), program halts.
 # It waits until user types his name and 'ENTER' in the console.
 # Then the control goes back to the program immediately, program proceeds.
-# input() function will read user input value AS A 'str', assigne it to your_name variable.
+# input() function will read user input value AS A 'str', assign it to your_name variable.
 # --------------------------------------------------------------------------
 print(f"Hello, {your_name}")
 print(f"Variable your_name's type is {type(your_name)}")
